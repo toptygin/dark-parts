89c28a50117709b80d6d0493bd864b7b8e0d81f6
unknown <Zlej robolovk@mobil.(none)>
2013-10-01 09:11:52 +0200

    Added the libGDX project skeleton.

---
+++ dark-parts-html/src/cz/derpest/darkparts/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package cz.derpest.darkparts.client;
+
+import cz.derpest.darkparts.DarkParts;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new DarkParts();
+	}
+}
